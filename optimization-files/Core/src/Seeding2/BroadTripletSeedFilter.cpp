b259695af1f37ed17b291d91e8953fb712d54dbd
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-26 15:20:31 +0200

    Revert "Use float fabs for radius compatibility"
    This reverts commit 916bb976347416a12bc22e6f473c939f83af99df.

--- optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
+++ optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
@@ -200,7 +200,7 @@ void BroadTripletSeedFilter::filterTripletTopCandidates(
       }
       // compared top SP should have at least deltaRMin distance
       float deltaR = currentTopR - otherTopR;
-      if (std::fabs(deltaR) < config().deltaRMin) {
+      if (std::abs(deltaR) < config().deltaRMin) {
         continue;
       }
       bool newCompSeed = true;
@@ -209,7 +209,7 @@ void BroadTripletSeedFilter::filterTripletTopCandidates(
         // seed (20mm instead of 5mm)
         // add new compatible seed only if distance larger than rmin to all
         // other compatible seeds
-        if (std::fabs(previousDiameter - otherTopR) < config().deltaRMin) {
+        if (std::abs(previousDiameter - otherTopR) < config().deltaRMin) {
           newCompSeed = false;
           break;
         }
