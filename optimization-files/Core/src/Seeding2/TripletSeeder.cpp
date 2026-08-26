471f688c2d9c7aa6c544f41b2322a7b59bb777ee
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-26 03:42:00 +0200

    Revert "remove empty-path triplet seeder logs"
    This reverts commit 0b1025bc3d9651fb8108484db6f5ba33519c7f5b.

--- optimization-files/Core/src/Seeding2/TripletSeeder.cpp
+++ optimization-files/Core/src/Seeding2/TripletSeeder.cpp
@@ -59,6 +59,7 @@ void createSeedsFromGroupsImpl(
 
   // no top SP found -> cannot form any triplet
   if (cache.topDoublets.empty()) {
+    ACTS_VERBOSE("No compatible Tops, returning");
     return;
   }
 
@@ -75,6 +76,7 @@ void createSeedsFromGroupsImpl(
 
   // no bottom SP found -> cannot form any triplet
   if (cache.bottomDoublets.empty()) {
+    ACTS_VERBOSE("No compatible Bottoms, returning");
     return;
   }
 
