2ebfecdce1c391630775f6eefef27e99b78dc590
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-26 10:21:47 +0200

    push sorted candidates into output

--- optimization-files/Core/src/Seeding2/detail/CandidatesForMiddleSp2.cpp
+++ optimization-files/Core/src/Seeding2/detail/CandidatesForMiddleSp2.cpp
@@ -83,10 +83,10 @@ void CandidatesForMiddleSp2::toSortedCandidates(
   std::ranges::sort_heap(m_indicesLow, comparator);
 
   for (const auto& [weight, index] : m_indicesHigh) {
-    output.emplace_back(m_storage[index]);
+    output.push_back(m_storage[index]);
   }
   for (const auto& [weight, index] : m_indicesLow) {
-    output.emplace_back(m_storage[index]);
+    output.push_back(m_storage[index]);
   }
 
   clear();
