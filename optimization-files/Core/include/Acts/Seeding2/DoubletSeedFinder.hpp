143e7d55217932d8ae58135fc803820e64f413ab
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-26 08:16:06 +0200

    avoid repeated doublet sort reserve call

--- optimization-files/Core/include/Acts/Seeding2/DoubletSeedFinder.hpp
+++ optimization-files/Core/include/Acts/Seeding2/DoubletSeedFinder.hpp
@@ -94,7 +94,6 @@ class DoubletsForMiddleSp {
   void sortByCotTheta(const IndexRange& range,
                       std::vector<IndexAndCotTheta>& indexAndCotTheta) const {
     indexAndCotTheta.clear();
-    indexAndCotTheta.reserve(range.second - range.first);
     for (Index i = range.first; i < range.second; ++i) {
       indexAndCotTheta.emplace_back(i, m_cotTheta[i]);
     }
