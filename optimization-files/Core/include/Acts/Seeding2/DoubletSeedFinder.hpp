839bbc7332702c7b3e62ac6321c10d9f6acec55e
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-26 07:46:54 +0200

    skip sorting singleton doublet ranges

--- optimization-files/Core/include/Acts/Seeding2/DoubletSeedFinder.hpp
+++ optimization-files/Core/include/Acts/Seeding2/DoubletSeedFinder.hpp
@@ -98,9 +98,12 @@ class DoubletsForMiddleSp {
     for (Index i = range.first; i < range.second; ++i) {
       indexAndCotTheta.emplace_back(i, m_cotTheta[i]);
     }
-    std::ranges::sort(indexAndCotTheta, {}, [](const IndexAndCotTheta& item) {
-      return item.cotTheta;
-    });
+    if (indexAndCotTheta.size() > 1) {
+      std::ranges::sort(indexAndCotTheta, {},
+                        [](const IndexAndCotTheta& item) {
+                          return item.cotTheta;
+                        });
+    }
   }
 
   /// Proxy accessor for a single doublet entry.
