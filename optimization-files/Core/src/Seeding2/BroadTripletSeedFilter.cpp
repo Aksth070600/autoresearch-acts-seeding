77a3f03b7e0b31b66e466799045dc75a79769659
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-27 21:22:52 +0200

    Revert "Order filter curvatures through explicit heaps"
    This reverts commit 8f0f4b93addfa859a7695c721ece98266712e24e.

--- optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
+++ optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
@@ -143,11 +143,10 @@ void BroadTripletSeedFilter::filterTripletTopCandidates(
   // initialize original index locations
   cache().topSpIndexVec.resize(tripletTopCandidates.size());
   std::iota(cache().topSpIndexVec.begin(), cache().topSpIndexVec.end(), 0);
-  const auto curvature = [&tripletTopCandidates](const std::size_t index) {
-    return tripletTopCandidates.curvatures()[index];
-  };
-  std::ranges::make_heap(cache().topSpIndexVec, {}, curvature);
-  std::ranges::sort_heap(cache().topSpIndexVec, {}, curvature);
+  std::ranges::sort(cache().topSpIndexVec, {},
+                    [&tripletTopCandidates](const std::size_t t) {
+                      return tripletTopCandidates.curvatures()[t];
+                    });
 
   // vector containing the radius of all compatible seeds
   cache().compatibleSeedR.reserve(config().compatSeedLimit);
