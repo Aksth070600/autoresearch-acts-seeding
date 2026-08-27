2ff00df449d45de5f9be74163ced533a904fc4d0
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-28 00:54:24 +0200

    Revert "Select filter curvatures in place"
    This reverts commit 831805aa0c5ba891a0ddda1db01274dddeab7108.

--- optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
+++ optimization-files/Core/src/Seeding2/BroadTripletSeedFilter.cpp
@@ -143,15 +143,10 @@ void BroadTripletSeedFilter::filterTripletTopCandidates(
   // initialize original index locations
   cache().topSpIndexVec.resize(tripletTopCandidates.size());
   std::iota(cache().topSpIndexVec.begin(), cache().topSpIndexVec.end(), 0);
-  for (auto position = cache().topSpIndexVec.begin();
-       position != cache().topSpIndexVec.end(); ++position) {
-    const auto smallest = std::ranges::min_element(
-        position, cache().topSpIndexVec.end(), {},
-        [&tripletTopCandidates](const std::uint32_t index) {
-          return tripletTopCandidates.curvatures()[index];
-        });
-    std::iter_swap(position, smallest);
-  }
+  std::ranges::sort(cache().topSpIndexVec, {},
+                    [&tripletTopCandidates](const std::size_t t) {
+                      return tripletTopCandidates.curvatures()[t];
+                    });
 
   // vector containing the radius of all compatible seeds
   cache().compatibleSeedR.reserve(config().compatSeedLimit);
