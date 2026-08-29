c4f4fa64a9ef9f09b9dcf67b6685361d3c3feecd
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-29 19:24:20 +0200

    Revert "Pack v4 core space point variances together"
    This reverts commit 636a31d0c2f4b1024b39372d8eb7280854c16016.

--- optimization-files/Core/src/Seeding2/TripletSeedFinder.cpp
+++ optimization-files/Core/src/Seeding2/TripletSeedFinder.cpp
@@ -150,9 +150,8 @@ class Impl final : public TripletSeedFinder {
       const DoubletsForMiddleSp::Proxy& bottomDoublet, TopDoublets& topDoublets,
       TripletTopCandidates& tripletTopCandidates) const {
     const float rM = spM.zr()[1];
-    const auto& varianceZRM = spM.varianceZR();
-    const float varianceZM = varianceZRM[0];
-    const float varianceRM = varianceZRM[1];
+    const float varianceZM = spM.varianceZ();
+    const float varianceRM = spM.varianceR();
 
     // Reserve enough space, in case current capacity is too little
     tripletTopCandidates.reserve(tripletTopCandidates.size() +
@@ -283,9 +282,8 @@ class Impl final : public TripletSeedFinder {
     const float rM = spM.zr()[1];
     const float cosPhiM = spM.xy()[0] / rM;
     const float sinPhiM = spM.xy()[1] / rM;
-    const auto& varianceZRM = spM.varianceZR();
-    const float varianceZM = varianceZRM[0];
-    const float varianceRM = varianceZRM[1];
+    const float varianceZM = spM.varianceZ();
+    const float varianceRM = spM.varianceR();
 
     // Reserve enough space, in case current capacity is too little
     tripletTopCandidates.reserve(tripletTopCandidates.size() +
