251547edd4897bb1f7bbcbdb2d2df93ba4c2449b
Thomas Aksdal <72987217+Aksth070600@users.noreply.github.com>
2026-08-30 02:47:56 +0200

    Restore retained tree after v4c slot 30

--- optimization-files/Core/include/Acts/Seeding2/TripletSeeder.hpp
+++ optimization-files/Core/include/Acts/Seeding2/TripletSeeder.hpp
@@ -26,8 +26,10 @@ class TripletSeeder {
   /// Cache for storing intermediate results during triplet seeding to avoid
   /// reallocation.
   struct Cache {
-    /// Shared cache for top and bottom doublets associated with one middle.
-    DoubletsForMiddleSp doublets;
+    /// Cache for bottom doublets associated with middle space points
+    DoubletsForMiddleSp bottomDoublets;
+    /// Cache for top doublets associated with middle space points
+    DoubletsForMiddleSp topDoublets;
 
     /// Sorted container of bottom doublet indices with cotangent theta values
     std::vector<DoubletsForMiddleSp::IndexAndCotTheta> sortedBottoms;
